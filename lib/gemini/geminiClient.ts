2f745afd0fc95475c789dbd22759e48eddbe429f
Nithwin <vmnithwin@gmail.com>
2025-08-12 22:41:47 +0530

    Added

--- lib/gemini/geminiClient.ts
+++ lib/gemini/geminiClient.ts
@@ -1,16 +1,15 @@
-import { GoogleGenerativeAI, Schema } from "@google/generative-ai";
+import { GoogleGenerativeAI, Schema, SchemaType } from "@google/generative-ai";
 
-const API_KEY:any = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const API_KEY: any = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 export const model = new GoogleGenerativeAI(API_KEY).getGenerativeModel({
   model: "gemini-1.5-flash"
 });
 
 export const pickupLineSchema: Schema = {
-  type: "object",
+  type: SchemaType.OBJECT,
   properties: {
-    line: { type: "string" },
-    category: { type: "string" },
+    line: { type: SchemaType.STRING },
+    category: { type: SchemaType.STRING },
   },
   required: ["line", "category"],
 };
-
